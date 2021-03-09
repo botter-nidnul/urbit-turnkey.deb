55aa614d502cc18cee9923e919284a5cda159f9a
~botter-nidnul <botter-nidnul@protonmail.com>
2021-03-09 08:10:14 -0600

    logging

--- urbit-turnkey/urbit_boot_monitor.sh
+++ urbit-turnkey/urbit_boot_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ `runuser -u urbit -- urbit -xt -w $1 -k $2 -c /home/urbit/urbit_pier | tee /tmp/boot_urbit.log` ]]
+if [[ `runuser -u urbit -- urbit -xt -w $1 -k $2 -c /home/urbit/urbit_pier | tee /dev/tty1 /tmp/boot_urbit.log` ]]
     then
         echo "Urbit boot complete"
         touch /home/urbit/.urbit_booted
